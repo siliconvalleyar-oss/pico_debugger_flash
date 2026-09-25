c65c450425930879dc8389e5754cee2cfeb7f2a9
silicon <siliconvalleyar@gmail.com>
2026-09-24 22:01:25 -0300

    Add extern C to app.h for shell.c linkage

--- floppydisk_usb/app.h
+++ floppydisk_usb/app.h
@@ -2,8 +2,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* UI: OLED menu (LEER / ESCRIBIR / INFO) + floppy<->image transfer logic. */
 void app_run(void);
 void app_show_version(void);
 const char* app_get_version_str(void);
 uint32_t app_get_build_num(void);
+
+#ifdef __cplusplus
+}
+#endif
