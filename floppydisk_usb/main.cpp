b75f64f5da05c2c8e9a915ff8a6f9625506205d4
silicon <siliconvalleyar@gmail.com>
2026-09-25 21:44:42 -0300

    Fix Pico W LED support in main.cpp
    - Added conditional compilation for Pico W LED (CYW43_WL_GPIO_LED_PIN)
    - Uses cyw43_arch for Pico W LED control
    - Falls back to PICO_DEFAULT_LED_PIN for regular Pico
    - Added pico_cyw43_arch to target_link_libraries

--- floppydisk_usb/main.cpp
+++ floppydisk_usb/main.cpp
@@ -2,11 +2,22 @@
 #include "shell.h"
 #include "pico/stdlib.h"
 #include "ssd1306.h"
+#include "pico/cyw43_arch.h"
 
 int main(void) {
     stdio_init_all();
     
     /* Quick LED blink to show we're alive */
+#if defined(CYW43_WL_GPIO_LED_PIN)
+    /* Pico W - LED is on wireless chip */
+    cyw43_arch_init();
+    for (int i = 0; i < 3; i++) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        busy_wait_ms(100);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        busy_wait_ms(100);
+    }
+#elif defined(PICO_DEFAULT_LED_PIN)
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     for (int i = 0; i < 3; i++) {
@@ -15,6 +26,7 @@ int main(void) {
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
         busy_wait_ms(100);
     }
+#endif
     
     ssd1306_init();
     shell_init();
